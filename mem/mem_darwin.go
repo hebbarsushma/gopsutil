368eac4d50045abf5e43d560560fe6d046da5d85
sushmahebbar <sushma.m@kloud9.nyc>
2018-08-06 13:42:21 +0530

    third commit

--- mem/mem_darwin.go
+++ mem/mem_darwin.go
@@ -8,7 +8,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/shirou/gopsutil/internal/common"
+	"github.com/hebbarsushma/gopsutil/internal/common"
 	"golang.org/x/sys/unix"
 )
 
--- mem/mem_linux.go
+++ mem/mem_linux.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/shirou/gopsutil/internal/common"
+	"github.com/hebbarsushma/gopsutil/internal/common"
 	"golang.org/x/sys/unix"
 )
 
